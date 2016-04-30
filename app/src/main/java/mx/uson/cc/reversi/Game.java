7f4f943957f5fa7da7c1644fc10c54419a059c55
Ernesto Jaramillo <nethojs29@gmail.com>
2016-04-29 18:04:44 -0700

    Nav buttons ya se esconden xD

--- app/src/main/java/mx/uson/cc/reversi/Game.java
+++ app/src/main/java/mx/uson/cc/reversi/Game.java
@@ -1,5 +1,7 @@
 package mx.uson.cc.reversi;
 
+import android.view.View;
+
 import mx.uson.cc.gameframework.Screen;
 import mx.uson.cc.gameframework.impl.AndroidGame;
 
@@ -9,6 +11,11 @@ import mx.uson.cc.gameframework.impl.AndroidGame;
 public class Game extends AndroidGame {
     @Override
     public Screen getStartScreen() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        
         return new LoadingScreen(this);
     }
 }
